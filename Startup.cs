52ef76839b931b57fecc73838b0092aeaadd943f
trongbang <dotrongbang.dk@gmail.com>
2017-04-14 13:07:08 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DA_THPT.Startup))]
+namespace DA_THPT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
